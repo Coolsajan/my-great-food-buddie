8a88796c43c0278ac09efaf3a4c8a6ab48eff2b1
Sajan Thapa <tsajan001@gmail.com>
2025-05-13 23:46:56 +0545

    minor update

--- app/data_ingestion/tripadviser_puller.py
+++ app/data_ingestion/tripadviser_puller.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
-from googlesearch import search
 from dotenv import load_dotenv
 from pathlib import Path
 import sys,os
