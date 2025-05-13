1a35333150cfdd61a47a414bd6aa8968d10196f9
Sajan Thapa <tsajan001@gmail.com>
2025-05-14 00:04:20 +0545

    sqlite3 update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from app.recommender import retrive_generate, check_dB_data
 import time
